aedc5ec44a5b55eab0e28aa9a64bb29bb81f41e8
Sahil Kourav <117891772+ijsahil@users.noreply.github.com>
2025-09-06 08:54:13 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,8 @@
 # app.py
 # This is a test commit
-def add(a, b, c, d):
-    return a + b + c + d
-
+def add(a, b, c, d, e):
+    return a + b + c + d + e
+ 
 def test_add():
-    assert add(1, 2, 3, 4) == 10
-    assert add(1, -1, 4 , -2) == 2
+    assert add(1, 2, 3, 4, 2) == 12
+    assert add(1, -1, 4 , -2, 1) == 3
